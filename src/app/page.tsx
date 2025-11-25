2609c69c75de440378476d57c03d824c49ced5ff
sharifuddinBD <sharifuddin331080@gmail.com>
2025-11-25 08:59:10 +0600

    Fix Parenting Problem

---
+++ src/app/page.tsx
@@ -0,0 +1,14 @@
+"use client"
+import React from 'react'
+import styles from './page.module.css'
+import Image from 'next/image'
+import { useRouter } from 'next/navigation'
+
+
+const page = () => {
+  return (
+    <div>page</div>
+  )
+}
+
+export default page
